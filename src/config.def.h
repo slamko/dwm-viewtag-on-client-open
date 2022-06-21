3a90be3afbe72f55a67aaa0c754471f938e9cf6b
Viacheslav <vaceslavkozin619@gmail.com>
2022-06-21 14:24:50 +0200

    Generated diff file

--- src/config.def.h
+++ src/config.def.h
@@ -26,9 +26,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating  view tag   monitor */
-	{ "Gimp",           NULL,       NULL,       0,            1,          1,         -1 },
-	{ "Brave-browser",  NULL,       NULL,       1 << 2,       0,          1,         -1 },
+	/* class      instance    title       tags mask     isfloating    view tag on client open     monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,            1,                          -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,            1,                          -1 },
 };
 
 /* layout(s) */
